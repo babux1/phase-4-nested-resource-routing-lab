f036b35ef0ce6adea464d8fa7ba474d794daa905
Batmunkh Namshir <namshir17@gmail.com>
2022-03-16 11:35:50 -0700

    commit tree e8380c72e4e6f80e2c54b18d929c6d69c824c2d1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
+  resources :items, only: [:index] do
   resources :users, only: [:show]
+end
+
+resources :users, only: [:show, :index, :create]
+
 end
